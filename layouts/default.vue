20c53dfea26e17ffd334d9a13d9827b56db43aab
ryo-hagiwara-fate <hagiwara-ryo2000@docomo.ne.jp>
2021-09-09 08:10:57 +0900

    add sample app

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,6 +105,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-camera',
+          title: 'SamplePage',
+          to: '/sample'
         }
       ],
       miniVariant: false,
